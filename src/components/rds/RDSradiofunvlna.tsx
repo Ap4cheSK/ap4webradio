b0b30c24ebdd71888cf9e824ff23708b0187de86
Ap4cheSK <apache@atlas.sk>
2023-08-21 12:14:41 +0200

    fixed funvlna

--- src/components/rds/RDSradiofunvlna.tsx
+++ src/components/rds/RDSradiofunvlna.tsx
@@ -6,8 +6,8 @@ async function RDSfunvlna(rdsUrl: rdsFormat) {
 	const fetchRDS = await fetch(rdsUrl.rdsUrl);
 	const xmlParser = new DOMParser();
 	const rdsParse = xmlParser.parseFromString(await fetchRDS.text(), "text/xml");
-	const rdsData = (rdsParse.getElementsByTagName("interpret")[0].childNodes[0].nodeValue + " - " + rdsParse.getElementsByTagName("skladba")[1].childNodes[0].nodeValue).normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+	const rdsData = (rdsParse.getElementsByTagName("interpret")[1].childNodes[0].nodeValue + " - " + rdsParse.getElementsByTagName("skladba")[1].childNodes[0].nodeValue).normalize("NFD").replace(/[\u0300-\u036f]/g, "")
 	return (rdsData);
 }
 
-export default RDSfunvlna
+export default RDSfunvlna;
