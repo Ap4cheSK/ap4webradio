2798d03515225b6b469865153887c812c642242b
Ap4cheSK <apache@atlas.sk>
2023-08-29 12:02:07 +0200

    PWA app support;

---
+++ src/serviceWorker.ts
@@ -0,0 +1,13 @@
+function registerServiceWorker() {
+	if("serviceWorker" in navigator) {
+		window.addEventListener("load", () => {
+			navigator.serviceWorker.register("/service-worker.js").then(registration => {
+				console.log("Service worker registrated with scope:", registration.scope);
+			}).catch(error => {
+				console.error("Service worker registration failed:", error);
+			});
+		});
+	}
+}
+
+export default registerServiceWorker;
